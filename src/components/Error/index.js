df68e4dd3b4fb1f6605c85cd3f3ff86de64ad92e
MatFabi <matseb.fabiszewski@gmail.com>
2020-11-03 16:32:59 +0100

    avoid use of deprecated method componentWillMount in CustomError component

--- src/components/Error/index.js
+++ src/components/Error/index.js
@@ -4,14 +4,9 @@ import { ErrorMessage } from "./styles";
 import { ModalLikeBg } from "components/PageSection";
 
 export default class CustomError extends React.Component {
-  state = {
-    error: null,
-  };
-
-  componentWillMount() {
-    if (this.props.error) {
-      this.setState({ error: true });
-    }
+  constructor(props) {
+    super(props);
+    this.state = { error: this.props.error };
   }
 
   render() {
